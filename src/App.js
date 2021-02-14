5587e62c2e854c14ad410f8dc1634477c2edb662
Orlando Montoya <omb0320@gmail.com>
2021-02-14 00:31:28 -0500

    Configure bootstrap on project

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import Home from './pages/Home'
 import { Login } from './pages/Login'
 import Register from './pages/Register'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
