6506933b2dc7c3b15036580578b5a5806ee9e05f
Orlando Montoya <omb0320@gmail.com>
2021-02-14 00:48:24 -0500

    Modify login and register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,12 +3,14 @@ import '../App.css';
 import React, { useState } from 'react';
 import { soccerServer } from '../utils/apiaxios'
 import { useHistory } from "react-router-dom";
+import { LinkContainer } from 'react-router-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 
 export function Login() {
 
   const history = useHistory()
-    
+
   const [ loginForm, setLoginForm ] = useState({
     email: "",
     password: ""
@@ -38,42 +40,49 @@ export function Login() {
       history.push('/')
     } 
     catch(err) {
-        console.log(err.message)
+      console.log(err.message)
     } 
   }
-
 
   const { email, password } = loginForm
 
   return (
-    <form onSubmit={handleLogin}>
-      <label htmlFor="email">Email</label>
-      <input 
-        type="email"
-        id="email"
-        name="email"
-        value={email}
-        required
-        onChange={handleChange}
-      />
-      <br></br>
-      <br></br>
-      
-      <label htmlFor="password">Password</label>
-      <input 
-        type="password"
-        id="password"
-        name="password"
-        value={password}
-        required
-        onChange={handleChange}
-      />
-      <br></br>
-      <br></br>
+    <div className="App">
+      <form onSubmit={handleLogin}>
+        <label htmlFor="email">Email</label>
+        <input 
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          required
+          onChange={handleChange}
+        />
+        <br></br>
+        <br></br>
+        
+        <label htmlFor="password">Password</label>
+        <input 
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          required
+          onChange={handleChange}
+        />
+        <br></br>
+        <br></br>
 
-      <button type="submit">
-        Login
-      </button>
-    </form>
+        <button type="submit">
+          Login
+        </button>
+
+        <br />
+        <br />
+        <LinkContainer to="/register" variant="dark">
+          <Button>Register</Button>
+        </LinkContainer>
+      </form>
+    </div>
   )
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 import { soccerServer } from '../utils/apiaxios'
+import Button from 'react-bootstrap/Button';
+import { LinkContainer } from 'react-router-bootstrap';
 
 
 const Register = () => {
@@ -32,35 +34,44 @@ const Register = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email</label>
-      <input 
-        type="email"
-        id="email"
-        name="email"
-        onChange={handleChage}
-        value={user.email}
-        required
-      />
-      <br></br>
-      <br></br>
-      
-      <label htmlFor="password">Password</label>
-      <input 
-        type="password"
-        id="password"
-        name="password"
-        onChange={handleChage}
-        value={user.password}
-        required
-      />
-      <br></br>
-      <br></br>
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input 
+          type="email"
+          id="email"
+          name="email"
+          onChange={handleChage}
+          value={user.email}
+          required
+        />
+        <br></br>
+        <br></br>
+        
+        <label htmlFor="password">Password</label>
+        <input 
+          type="password"
+          id="password"
+          name="password"
+          onChange={handleChage}
+          value={user.password}
+          required
+        />
+        <br></br>
+        <br></br>
 
-      <button type="submit">
-        Register
-      </button>
-    </form>
+        <button type="submit">
+          Register
+        </button>
+
+        <br />
+        <br />
+
+        <LinkContainer to="/login" variant="info">
+          <Button>Login</Button>
+        </LinkContainer>
+      </form>
+    </div>
   )
 }
 
