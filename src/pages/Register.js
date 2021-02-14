181b2d6ef0b31c473696a7a865846329065b52e4
Esteban Silva <esilvac59@gmail.com>
2021-02-13 19:57:57 -0500

    Adding register component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,38 +1,62 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from "react-router-dom";
+import { Box, Button } from '@material-ui/core';
 
-// import '../App.css';
+const Register = () => {
+  const [user, setUser] = useState({ email: '', password: '' })
+  const history = useHistory()
 
-// export function Register() {
-//   return (
-//     <div className="App">
-//       <h1>Register</h1>
-//     </div>
-//   )
-// }
+  const handleSubmit = async (event) => {
+    event.preventDefault()
 
+    try {
+      const res = await axios.post('http://localhost:8000/users', user)
 
-export function Register({ email,  password, handleChage, handleSubmit }) {
+      sessionStorage.setItem('token', res.token)
+      history.push('/')
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  const handleChage = ({ target: { name, value } }) => {
+    setUser({ ...user, [name]: value }) 
+  }
+
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email</label>
-      <input 
-        type="email"
-        id="email"
-        name="email"
-        onChange={handleChage}
-        value={email}
-        required
-      />
+      <Box 
+        display="flex"
+        justifyContent="center"
+        flexDirection="column"
+      >
+        <label htmlFor="email">Email</label>
+        <input 
+          type="email"
+          id="email"
+          name="email"
+          onChange={handleChage}
+          value={user.email}
+          required
+        />
 
-      <label htmlFor="password">Password</label>
-      <input 
-        type="password"
-        id="password"
-        name="password"
-        onChange={handleChage}
-        value={password}
-        required
-      />
-      <button>Register</button>
+        <label htmlFor="password">Password</label>
+        <input 
+          type="password"
+          id="password"
+          name="password"
+          onChange={handleChage}
+          value={user.password}
+          required
+        />
+
+        <Button variant="contained" color="primary" type="submit">
+          Primary
+        </Button>
+      </Box>
     </form>
   )
 }
+
+export default Register
