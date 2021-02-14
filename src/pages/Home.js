be5e0842620c1524e7c78ff9c481225de2380910
Orlando Montoya <omb0320@gmail.com>
2021-02-13 23:49:51 -0500

    Add logout function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,23 @@
 
 import '../App.css';
+import { useHistory } from "react-router-dom";
+
 
 function Home() {
+  const history = useHistory()
+
+  const handleLogout = () => {
+    sessionStorage.clear()
+    history.push('/register')
+  }
+
   return (
     <div className="App">
       <h1>Home</h1>
+      <button
+        onClick={handleLogout}
+      > Logout
+      </button>
     </div>
   )
 }
