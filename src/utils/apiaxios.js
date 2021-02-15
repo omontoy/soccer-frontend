a89de4604826d2c5f6db4045ca84bd3c8f092db0
Orlando Montoya <omb0320@gmail.com>
2021-02-15 01:09:28 -0500

    Add apiFootball env variables and api server

--- src/utils/apiaxios.js
+++ src/utils/apiaxios.js
@@ -3,3 +3,11 @@ import axios from 'axios';
 export const soccerServer = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL
 });
+
+export const apiFootball = axios.create({
+  baseURL: process.env.REACT_APP_API_SERVER_URL,
+  headers: {
+    'x-rapidapi-host': process.env.REACT_APP_X_RAPIDAPI_HOST,
+    'x-rapidapi-key': process.env.REACT_APP_X_RAPIDAPI_KEY 
+  }
+})
